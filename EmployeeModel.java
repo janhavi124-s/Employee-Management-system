e474ef80ee891e6bd590a53a17dfe9e41bbf2968
Janhavi P.  Somnathe <janhavisomnathe@gmail.com>
2025-07-13 16:29:06 +0530

    Create EmployeeModel.java

---
+++ EmployeeModel.java
@@ -0,0 +1,54 @@
+//employee model with name Employee
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "department")
+    private String department;
+    
+    // getters and setters for the employee model
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+}
