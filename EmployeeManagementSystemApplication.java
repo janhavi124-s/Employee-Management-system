551a77bddc176a3226756b9c55dff7e6d6696fe5
Janhavi P.  Somnathe <janhavisomnathe@gmail.com>
2025-07-13 16:03:15 +0530

    Create EmployeeManagementSystemApplication.java

---
+++ EmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//this is a main class for the employee management system
+
+@SpringBootApplication
+public class EmployeeManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
+}
